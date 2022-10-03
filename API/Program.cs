50e78443ad69a4dcb2e41b3b42b21683b6d6a4cf
Yogesh Koli <info@yogeshkoli.com>
2022-10-03 20:04:14 +0530

    Creating a database seeder - A database seeder - Running it on application startup - the Startup confguration / Host configuration - log confugration with execption handling on startup

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,24 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>(); // catch the exception and log them 
+            try
+            {
+                context.Database.Migrate(); // this going to exeute the database update command on the startup 
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "problem migratiing data");
+            }
+            // finally{
+            //     scope.Dispose(); // Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+            // }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
