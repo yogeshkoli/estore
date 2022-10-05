574ee67bab713501cbbc718b57300c2530753ba3
Yogesh Koli <info@yogeshkoli.com>
2022-10-05 17:02:45 +0530

    Converting thread based database request to use async methods to query database

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -12,25 +13,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
         public ProductController(StoreContext context)
         {
-            this.context = context;
-            
+            _context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
-
-            return Ok(products);
-        }
+            return await _context.Products.ToListAsync();
+        } 
 
         [HttpGet("{id}")] // api/products/1
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 } 
