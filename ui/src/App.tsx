1acbb7550051f7b24e64887be9e1ac4c926a1433
Yogesh Koli <info@yogeshkoli.com>
2022-10-06 19:33:31 +0530

    Using the .Net API in React App

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [products, setProducts] = useState([
+    {
+      name: 'product', price: 100
+    }
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/Product')
+    .then(r => r.json())
+    .then(data => setProducts(data));
+  }, []);
+
   return (
     <div>
        <h1>EStore</h1>
+       <ul>
+        {
+          products.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))
+        }
+       </ul>
     </div>
   );
 }
