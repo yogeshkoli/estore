d431ba46de8561d0460abb0440fd0c6d3d7f6fb2
Yogesh Koli <info@yogeshkoli.com>
2022-10-06 19:02:45 +0530

    Config change to use CORS and allow the http origin

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,8 @@ namespace API
             services.AddDbContext<StoreContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(opt =>{
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000/");
+            });
 
             app.UseAuthorization();
 
