7a620763f6a8ad80a90d21a59c2e35fb9af20c29
Yogesh Koli <info@yogeshkoli.com>
2022-10-10 18:03:37 +0530

    Navigate to product details page

--- ui/src/features/catalog/ProductCard.tsx
+++ ui/src/features/catalog/ProductCard.tsx
@@ -1,43 +1,44 @@
 import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography, Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
 import { Product } from "../../app/model/product";
 
-interface Props{
-    product: Product
+interface Props {
+  product: Product
 }
 
-export default function ProductCard({product} : Props){
-    return(
-        <Card>
-          <CardHeader
-          avatar={
-            <Avatar sx={{bgcolor: 'secondary.main'}}>
-              {product.name.charAt(0).toUpperCase()}
-            </Avatar>
-          }
-          title={product.name}
-          titleTypographyProps={{
-            sx: {fontWeight: 'bold', color: 'primary.main'}
-          }}
-          />
-        <CardMedia
-          sx={{backgroundSize: 'contain', bgcolor: 'primary.light'}}
-          component="img"
-          height="140"
-          image={product.pictureUrl}
-          alt={product.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" color='secondary'>
-            $ {(product.price / 100).toFixed(2)} 
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
+export default function ProductCard({ product }: Props) {
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: 'secondary.main' }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: 'bold', color: 'primary.main' }
+        }}
+      />
+      <CardMedia
+        sx={{ backgroundSize: 'contain', bgcolor: 'primary.light' }}
+        component="img"
+        height="140"
+        image={product.pictureUrl}
+        alt={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" color='secondary'>
+          $ {(product.price / 100).toFixed(2)}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
           {product.brand} / {product.type}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Add to Cart</Button>
-          <Button size="small">View</Button>
-        </CardActions>
-      </Card>
-    )
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to Cart</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
+      </CardActions>
+    </Card>
+  )
 } 
