e530c3cb3df7a893c1a22d896f8f3e177debc1d7
Yogesh Koli <info@yogeshkoli.com>
2022-10-12 10:25:32 +0530

    using axios interceptors to handle user side errors

--- ui/src/app/api/agent.ts
+++ ui/src/app/api/agent.ts
@@ -1,8 +1,34 @@
-import axios, {AxiosResponse} from 'axios';
+import axios, {AxiosError, AxiosResponse} from 'axios';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.response.use(response => {
+    return response;
+}, (error: any) => {
+    const {data, status} = error.response!;
+
+    switch (status) {
+        case 400:
+            toast.error(data.title);
+            break;
+        case 401:
+            toast.error(data.title);
+            break;
+        case 500:
+            toast.error(data.title);
+            break;
+        case 500:
+            toast.error(data.title);
+            break;
+    
+        default:
+            break;
+    }
+    return Promise.reject(error.response);
+});
 
 const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
