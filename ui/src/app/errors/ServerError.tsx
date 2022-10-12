1de4474d7003c543ea97e0d49b5216763c7b77c1
Yogesh Koli <info@yogeshkoli.com>
2022-10-12 11:55:41 +0530

    the server error page design with back button to go to catalog page

--- ui/src/app/errors/ServerError.tsx
+++ ui/src/app/errors/ServerError.tsx
@@ -1,5 +1,5 @@
-import { Container, Paper, Typography } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import { Button, Container, Divider, Paper, Typography } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
 
 export default function ServerError()
 {
@@ -11,9 +11,16 @@ export default function ServerError()
                 state?.error ? (
                     <>
                         <Typography variant="h5" gutterBottom>Server Error</Typography>
+                        <Divider />
+                        <Typography>
+                            {
+                                state.error.detail || 'Internal Server Error'
+                            }
+                        </Typography>
                     </>
-                ) : ''
+                ) : <Typography variant="h5" gutterBottom>Server Error</Typography>
             }
+            <Button component={Link} to="/catalog">Go back to the store</Button>
         </Container>
     )
 }
