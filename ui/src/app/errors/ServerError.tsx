03e0b32ce636e843c06602dd0425fb7ed81ee9d3
Yogesh Koli <info@yogeshkoli.com>
2022-10-12 11:50:54 +0530

    Getting the state from the react location which was passed from the navigation

--- ui/src/app/errors/ServerError.tsx
+++ ui/src/app/errors/ServerError.tsx
@@ -1,10 +1,19 @@
 import { Container, Paper, Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 export default function ServerError()
 {
+    const {state}  = useLocation();
+ 
     return(
         <Container component={Paper}>
-            <Typography variant="h5" gutterBottom>Server Error</Typography>
+            {
+                state?.error ? (
+                    <>
+                        <Typography variant="h5" gutterBottom>Server Error</Typography>
+                    </>
+                ) : ''
+            }
         </Container>
     )
 }
