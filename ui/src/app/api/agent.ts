35f954a4a3b513722bd4db48d5df60edcf35daa9
Yogesh Koli <info@yogeshkoli.com>
2022-10-12 10:40:19 +0530

    Catching and listing the validation errors

--- ui/src/app/api/agent.ts
+++ ui/src/app/api/agent.ts
@@ -12,6 +12,18 @@ axios.interceptors.response.use(response => {
 
     switch (status) {
         case 400:
+            if(data.errors)
+            {
+                const modelStateErrors: string[] = [];
+                for(const key in data.errors)
+                {
+                    if(data.errors[key])
+                    {
+                        modelStateErrors.push(data.errors[key]);
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
             toast.error(data.title);
             break;
         case 401:
